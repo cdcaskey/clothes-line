ef49b388041ee545f8a9f7c55208892bc3a94fc8
Chris Caskey <cdcaskey@users.noreply.github.com>
2022-02-16 00:30:56 +0000

    Add ability to copy/share join link

---
+++ ClothesLine/wwwroot/js/Session.js
@@ -0,0 +1,36 @@
+﻿const getDeviceType = () => {
+    const ua = navigator.userAgent;
+    if (/(tablet|ipad|playbook|silk)|(android(?!.*mobi))/i.test(ua)) {
+        return "tablet";
+    }
+    if (
+        /Mobile|iP(hone|od)|Android|BlackBerry|IEMobile|Kindle|Silk-Accelerated|(hpw|web)OS|Opera M(obi|ini)/.test(
+            ua
+        )
+    ) {
+        return "mobile";
+    }
+    return "desktop";
+};
+
+const canShare = () => {
+    return navigator.canShare();
+}
+
+function showCopy() {
+    return !canShare() || getDeviceType() == "desktop";
+}
+
+function shareLink(title, text, url) {
+    let shareData = {
+        title: title,
+        text: text,
+        url: url
+    };
+
+    navigator.share(shareData);
+}
+
+function copyText(text) {
+    navigator.clipboard.writeText(text);
+}
