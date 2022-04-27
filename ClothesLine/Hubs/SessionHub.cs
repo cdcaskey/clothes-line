8c376c58a98ea9f25a5df81f7ffa1a2ad5e0d8aa
Chris Caskey <cdcaskey@users.noreply.github.com>
2022-04-27 22:20:41 +0100

    Add the ability to spectate a session
    This commit allows a user to join a session as a spectator, as well as
    to drop in and out of spectating in the middle of a session
    
    Resolves #13

--- ClothesLine/Hubs/SessionHub.cs
+++ ClothesLine/Hubs/SessionHub.cs
@@ -16,10 +16,17 @@ namespace ClothesLine.Hubs
             this.connections = connections;
         }
 
-        public async Task JoinSession(string sessionId, string name)
+        public async Task JoinSession(string sessionId, string name, bool spectating)
         {
-            connections.Add(Context.ConnectionId, sessionId, name);
+            connections.Add(Context.ConnectionId, sessionId, name, spectating);
             await Groups.AddToGroupAsync(Context.ConnectionId, sessionId);
+            
+            await UpdateGroup(sessionId);
+        }
+
+        public async Task SetSpectating(string sessionId, bool spectating)
+        {
+            connections.SetSpectating(Context.ConnectionId, spectating);
             await UpdateGroup(sessionId);
         }
 
