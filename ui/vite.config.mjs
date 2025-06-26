00ebcb5f4c7355ba8704fc8b9e949745a7e56c44
Chris Caskey <chris.caskey@outlook.com>
2025-06-27 00:52:37 +0100

    Deploy to gh pages

--- ui/vite.config.mjs
+++ ui/vite.config.mjs
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
+  base: '/clothes-line',
   plugins: [react(), tsconfigPaths()],
   test: {
     globals: true,
