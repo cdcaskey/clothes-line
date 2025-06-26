6f63c53719a50d0515d31d192f158802a45b7520
Chris Caskey <chris.caskey@outlook.com>
2025-06-26 22:42:10 +0100

    Create UI project

---
+++ ui/.storybook/main.ts
@@ -0,0 +1,17 @@
+import type { StorybookConfig } from '@storybook/react-vite';
+
+const config: StorybookConfig = {
+  core: {
+    disableWhatsNewNotifications: true,
+    disableTelemetry: true,
+    enableCrashReports: false,
+  },
+  stories: ['../src/**/*.mdx', '../src/**/*.story.@(js|jsx|ts|tsx)'],
+  addons: ['storybook-dark-mode'],
+  framework: {
+    name: '@storybook/react-vite',
+    options: {},
+  },
+};
+
+export default config;
---
+++ ui/src/components/ColorSchemeToggle/ColorSchemeToggle.tsx
@@ -0,0 +1,25 @@
+import { Button, Group, useMantineColorScheme } from '@mantine/core';
+
+export function ColorSchemeToggle() {
+  const { colorScheme, setColorScheme } = useMantineColorScheme();
+
+  return (
+    <Group justify="center" mt="xl">
+        { colorScheme == 'light' &&
+            <Button onClick={() => setColorScheme('dark')}>
+                <span className="material-symbols-outlined">light_mode</span>
+            </Button>
+        }
+        { colorScheme == 'dark' &&
+            <Button onClick={() => setColorScheme('auto')}>
+                <span className="material-symbols-outlined">dark_mode</span>
+            </Button>
+        }
+        { colorScheme == 'auto' &&
+            <Button onClick={() => setColorScheme('light')}>
+                <span className="material-symbols-outlined">night_sight_auto</span>
+            </Button>
+        }
+    </Group>
+  );
+}
