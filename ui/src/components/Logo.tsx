88638a5a1a5488b7317c5d8d7e6fdc82d24ebf11
Chris Caskey <chris.caskey@outlook.com>
2025-06-27 01:01:05 +0100

    fix logo

--- ui/src/components/Logo.tsx
+++ ui/src/components/Logo.tsx
@@ -2,7 +2,7 @@ import { Group, Text } from '@mantine/core';
 
 export function Logo({ size = 30 }: { size?: number }) {
     return (
-        <Group spacing="xs">
+        <Group>
             <Text size="xl" fw="700" variant="gradient" component="span" gradient={{ from: 'dodgerblue', to: 'blue' }}>
                 <span className="material-symbols-outlined">dry_cleaning</span>
                 Clothes Line
